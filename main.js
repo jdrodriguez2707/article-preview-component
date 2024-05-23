3da085b488573fa5fe9937fdf5071531070d8169
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-05-23 11:07:41 -0500

    Fix event listener to work without conflicts on mobile and desktop screens

--- main.js
+++ main.js
@@ -2,21 +2,33 @@ const shareIcon = document.querySelector("#share-icon");
 const authorSection = document.querySelector("#author-section");
 const shareSection = document.querySelector("#share-section");
 const shareIconActive = document.querySelector("#share-icon-active");
+const shareIconWhite = document.querySelector("#share-icon-white");
 
 shareIcon.addEventListener("touchstart", showShareSection);
 shareIcon.addEventListener("click", showShareSectionActive);
 shareIconActive.addEventListener("touchstart", showAuthorSection);
+shareIconWhite.addEventListener("click", hideShareSectionActive);
 
-function showShareSection() {
+function showShareSection(event) {
+  event.preventDefault();
   authorSection.classList.add("inactive");
   shareSection.classList.remove("inactive");
 }
 
-function showAuthorSection() {
+function showAuthorSection(event) {
+  event.preventDefault();
   shareSection.classList.add("inactive");
   authorSection.classList.remove("inactive");
 }
 
 function showShareSectionActive() {
   shareSection.classList.toggle("inactive");
+  shareIconWhite.classList.remove("inactive");
+  shareIcon.classList.add("inactive");
+}
+
+function hideShareSectionActive() {
+  shareSection.classList.add("inactive");
+  shareIconWhite.classList.add("inactive");
+  shareIcon.classList.remove("inactive");
 }
